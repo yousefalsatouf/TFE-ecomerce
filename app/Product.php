2ca0eedcac8354ed2e692ad2a70120f310a78893
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2020-02-24 15:24:59 +0100

    fields have been added to product model

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+    protected $table = 'products';
+    protected $primaryKey = 'id';
+    protected $fillable = ['product_name', 'product_code', 'product_price', 'image', 'product_info', 'qpl_price'];
+
+
 }
