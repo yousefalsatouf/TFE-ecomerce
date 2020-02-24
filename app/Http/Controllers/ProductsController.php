68af2d3972642277814e03f3be446d9e6b259a7a
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2020-02-24 16:11:07 +0100

    add product is done time to test it

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,9 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
     //
+    public function index()
+    {
+        $product = Product::all();
+
+        return view('admin.product.index', compact('products'));
+    }
+
+    public function create()
+    {
+        return view('admin.product.create');
+    }
+
+    public function store( Request $request)
+    {
+        $formInput = $request->except('image');
+        $this->validate($request, [
+            'product_name' => 'required',
+            'product_code' => 'required',
+            'product_price' => 'required',
+            'product_info' => 'required',
+            'spl_price' => 'required',
+            'image' => 'image|mimes:png,jpg,jpeg|max:10000'
+        ]);
+
+        $image = $request>image;
+        if ($image)
+        {
+            $imageName = $image->getClientOriginalName();
+            $image->move('images', $imageName);
+            $formInput['image']=$imageName;
+        }
+        Product::create($formInput);
+        return redirect()->back();
+    }
+
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+
+        return view('product.show', compact('products'));
+    }
 }
