b4b542f807474b34b2c315d50e2137f4804111af
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2020-02-24 09:37:25 +0100

    product table has been added to our database

---
+++ database/migrations/2020_02_24_082749_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('product_name');
+            $table->string('product_code');
+            $table->string('product_price');
+            $table->string('product_info');
+            $table->string('image')->nullable();
+            $table->string('spl_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
