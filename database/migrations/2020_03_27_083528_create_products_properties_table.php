8a5e5ba8fa6778e3b030d014d50698664328bd7b
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2020-03-27 09:39:40 +0100

    product properties table is created and migrated to the database

---
+++ database/migrations/2020_03_27_083528_create_products_properties_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsPropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products_properties', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('product_id');
+            $table->string('size');
+            $table->string('color');
+            $table->string('product_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products_properties');
+    }
+}
