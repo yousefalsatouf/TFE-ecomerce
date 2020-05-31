e06d154f4c314bf4fcb49950d16ee39ba685f406
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2020-05-31 21:50:57 +0200

    fixing some incomming changes after merging

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,11 +19,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-<<<<<<< HEAD
-            return redirect('front.home');
-=======
             return redirect(RouteServiceProvider::USER_HOME);
->>>>>>> test
         }
 
         return $next($request);
