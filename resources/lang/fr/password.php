0696ea4b515c9718525f179da1262e2cf998a894
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2020-10-04 08:19:24 +0200

    some troubles with forget password

---
+++ resources/lang/fr/password.php
@@ -0,0 +1,10 @@
+<?php
+
+
+return [
+    'reset' => 'Réinitialiser le mot de passe',
+    'email' => 'E-mail adresse',
+    'pass' => 'Mot de passe',
+    'sendLink' => 'Envoyer le lien de réinitialisation du mot de passe',
+    'confirm' => 'Confirmer le mot de passe'
+];
