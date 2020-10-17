cc7324224e18aea133031e151e154874d9b1be8f
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2020-10-17 09:38:22 +0200

    rgbd is created and works just fine

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -15,5 +15,51 @@ return [
     'managePass' => "Manage your password",
     'changePass' => "Change Password",
     'currPass' => "Current Password",
-    'newPass' => "New password"
+    'newPass' => "New password",
+    'rgbd' => [
+        'warning' => 'WARNING :
+                                 “The SPORTClub company processes your data in order to allow your registration on this site. To find out more about how your personal data is processed and stored as well as to check your rights, <b> please read the GDPR conditions and accept them by checking the small box that you will find in the popup that will open </b> (otherwise you will not be able to register).
+                                 Please click',
+        'infoTitle' => 'Please scroll down to read carefully and check the box to accept the conditions mentioned in this window:',
+        'check' => 'By checking this box, I accept the privacy policy of this site and I agree to the processing of my data for the proper use of this site.',
+        'content' => '
+            <div>
+    <p> The ULB attaches great importance to the protection of your personal data. </p>
+
+    <p> By registering on the Hedrine portal of the Faculty of Pharmacy of the Université Libre de Bruxelles (hereinafter "ULB"),
+        you agree that the personal data collected during registration (name, first name, email address, telephone number, encrypted password and name of your team / affiliation) be hosted in the Hedrine database for the proper functioning of this portal.
+    </p>
+
+    {{- <p> Your data is sent to the Order of Pharmacists of Belgium in order to verify your situation before granting approval.
+        Except to meet a legal obligation, they are not transmitted to third parties.
+    </p> -}}
+
+    {{- <p> In addition, once your accreditation has been obtained, your data may be used by the ULB to communicate with you about the students hosted in your pharmacy,
+        in particular to invite you to their proclamation ceremony.
+    </p> -}}
+
+    <p> ULB staff members who have access to your data are bound by an obligation of confidentiality, which implies in particular: </p>
+
+    <ul>
+        <li> not to access or seek to access data that is not strictly necessary for the performance of their function; </li>
+        <li> not to disclose, both during their employment relationship and after the end of it, any personal data of which they may have become aware in the performance of their duties, in addition to what may be necessary for the exercise of this; </li>
+        <li> not to keep any copy of the data processed at the end of their engagement with the ULB. </li>
+    </ul>
+
+    {{- <p> Students with access to your data are bound by similar confidentiality obligations. </p> -}}
+    <p> Here is the situation regarding the deletion of your personal data: </p>
+    <ul>
+        <li> When deleting an account from a reader (user who does not modify site data, but consults it), personal data will be immediately and permanently deleted from the database. </ li >
+        <li> In the event that the registration of a <b><u>lecteur</u> </b> is refused by an administrator, your data will be deleted from the Hedrine portal within 30 days of notification of the refusal </li>
+        <li> It is not the same for the people who participate in the life of this portal by updating the data (the users who modify the data of the site, therefore who have or will have a role of editor, of publisher or admin). Your personal data cannot be deleted from the database, under penalty of major malfunction of the portal. </li>
+    </ul>
+
+    <p> All your questions about the protection of your data by ULB can be sent to the Data Protection Officer: rgpd@ulb.ac.be. </p>
+
+    <p> This address is also the one through which you can exercise your rights in this matter, by justifying your identity: access, rectification, erasure, limitation, opposition and portability. </p>
+
+    <p> I have read and confirm that I have fully understood the provisions relating to the processing of my data, whether I am or become a reader, editor, publisher or admin (GDPR) user. </p>
+</div>
+        '
+    ]
 ];
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -54,15 +54,62 @@
 
                         <div class="form-group row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('auth.confirm') }}</label>
-
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
+                        <div class="modal fade" id="myModal" role="dialog">
+                            <div class="modal-dialog">
+                                <!-- Modal content-->
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h3 class="modal-title" id="exampleModalLabel">
+                                            <a href="{{url('/')}}">SPORTClub</a>
+                                        </h3>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <h4 class="text-success text-center">RGPD : SPORTClub</h4>
+                                        <hr>
+                                        <strong class="text-info"><i class="fa fa-info-circle">
+                                            </i>@lang('auth.rgbd.infoTitle')</strong>
+                                        <hr>
+                                        @include('auth.terms')
+                                        <hr>
+                                        <div class="condtions">
+                                            <!-- Button trigger modal -->
+                                            <label for="check-me" id="checkboxContent">
+                                                => <input type="checkbox" name="RGPD" id="check-me" class="@error('RGPD') is-invalid @enderror" required value="1"><=
+                                                @lang('auth.rgbd.check')
+                                            </label>
+                                            @error('RGPD')
+                                            <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer m-footer bg-light">
+                                        <input type="button" class="btn btn-outline-danger" data-dismiss="modal" required value="NO">
+                                        <i class="far fa-smile-wink"></i>
+                                        <input type="button" class="btn btn-outline-success" data-dismiss="modal" required value="OK" id="accept">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
+                        <div class="form-group row flex justify-content-center">
+                            <blockquote class="w-75">
+                                <i class="fa fa-info-circle info text-danger" id="required-msg"></i>
+                                @lang('auth.rgbd.warning')
+                                <button type="button" class="btn btn-outline-light text-dark awesome-font" data-toggle="modal" data-target="#myModal"><i class="fa fa-folder-open-o"></i> </button>»
+                            </blockquote>
+                        </div>
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit">
+                                <button type="submit" id="registerBtn">
                                     {{ __('auth.titleRegister') }}
                                 </button>
                             </div>
