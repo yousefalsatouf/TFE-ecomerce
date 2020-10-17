4fe0c2ee638b28d31c18f49e410b94ddc1b04075
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2020-10-17 08:33:58 +0200

    map is adding using iframe

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -1,12 +1,13 @@
 @extends('front.helpers.master')
 @section('content')
     <section id="gmap">
-        <gmap-map
+        {{--<gmap-map
             :center="{lat:10,lng:10}"
             :zoom="7"
             style="width: 100%; height: 500px"
         >
-        </gmap-map>
+        </gmap-map>--}}
+        <iframe src="https://www.google.com/maps/d/embed?mid=1cWzykfq-y4XYmcuZz5tXmZc9CFPr5C3R" width="100%" height="480"></iframe>
     </section>
     <section id="contact">
         <div class="container">
