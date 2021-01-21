4af27afdcea3bb70c597875b03f3bf4aaf5621c8
Yousef <32648333+yousefalsatouf@users.noreply.github.com>
2021-01-21 10:56:54 +0100

    working on changing role using vuejs

--- resources/js/admin/pages/Users.vue
+++ resources/js/admin/pages/Users.vue
@@ -3,40 +3,49 @@
      <md-card>
       <md-card-header data-background-color="black">
             <h4 class="title">Users Table</h4>
-            <p class="category">All Users listed in this table (you are the only one who has asscess and makes changes to this table).</p>
+            <p class="category">All Users listed in this table (you are the only one who has asscess and makes changes to this table.</p>
       </md-card-header>
       <md-card-content>
-            <md-table v-model="users" md-sort="name" md-sort-order="desc" md-card md-fixed-header>
+            <md-table v-model="users" md-sort="name" md-sort-order="desc">
                 <md-table-toolbar>
                       <div class="md-toolbar-section-start">
                           <h1 class="md-title">Users</h1>
                       </div>
                 </md-table-toolbar>
                 <md-table-row slot="md-table-row" slot-scope="{ item }">
-                      <md-table-cell md-label="Image" md-sort-by="image">
+                      <md-table-cell md-label="Image">
                         <img v-if="item.image" class="img" v-bind:src="`/images/${item.image}`" />
                         <i v-else class="material-icons" style="font-size: 50px">person</i>
                       </md-table-cell>
                       <md-table-cell md-label="Name" md-sort-by="name" class="name">{{ item.first_name }} {{item.last_name }}</md-table-cell>
-                      <md-table-cell md-label="Email" md-sort-by="email">{{ item.email }}</md-table-cell>
-                      <md-table-cell md-label="Phone" md-sort-by="phone">{{ item.phone_number }}</md-table-cell>
-                       <md-table-cell md-label="Email Verified? " md-sort-by="email-verified-at">
+                      <md-table-cell md-label="Email">{{ item.email }}</md-table-cell>
+                      <md-table-cell md-label="Phone">{{ item.phone_number? item.phone_number: "No phone number" }}</md-table-cell>
+                       <md-table-cell md-label="Email Verified? ">
                         <md-icon class="text-success" v-if="item.email_verified_at">check</md-icon>
                         <md-icon v-else class="text-danger">thumb_down_alt</md-icon> 
                       </md-table-cell>
-                      <md-table-cell md-label="Subscribed Newsletter" md-sort-by="newsletter">
+                      <md-table-cell md-label="Subscribed Newsletter">
                            <md-icon class="text-success" v-if="item.subscribed_newsletter===1">check</md-icon>
                            <md-icon v-else class="text-danger">thumb_down_alt</md-icon> 
                       </md-table-cell>
-                      <md-table-cell md-label="About him" md-sort-by="abouthim">
+                      <md-table-cell md-label="About him">
                         <p v-if="item.about">{{ item.about }}</p>
                         <strong class="text-danger" v-else>Nothing yet</strong>
                       </md-table-cell>
-                      <md-table-cell md-label="Role" md-sort-by="role">{{ item.admin? "Admin" : "Editor" }}</md-table-cell>
+                      <md-table-cell md-label="Role">
+                        <md-field id="role">
+                            <label for="role">Changing Role: </label>
+                            <md-select v-model="role" name="role" id="role" md-dense>
+                              <md-option value="admin">Admin</md-option>
+                              <md-option value="editor">Editor</md-option>
+                              <md-option value="user">User</md-option>
+                            </md-select>
+                          </md-field>
+                      </md-table-cell>
                       <md-table-cell>
-                          <md-button class="md-just-icon md-simple" @click="handle(item.id)">
-                          <md-icon>build</md-icon>
-                          <md-tooltip md-direction="top">change role</md-tooltip>
+                          <md-button class="md-just-icon md-simple" @click="handleRole(item.id)">
+                            <md-icon>build</md-icon>
+                            <md-tooltip md-direction="top">change role</md-tooltip>
                           </md-button>
                       </md-table-cell>
                 </md-table-row>
@@ -54,8 +63,16 @@ export default {
      name: "users-table",
       data() {
         return {
-          users: null
+          users: null,
+          role: null,
+         success: false,
      };
+    },
+    methods: {
+      handleRole(id)
+      {
+        
+      }
     },
     created(){
       axios.get('/admin/users').then(res => this.users= res.data).catch(err => console.log(err))
@@ -65,6 +82,13 @@ export default {
 </script>
 
 <style lang="scss">
+    #role
+    {
+      ul
+      {
+        color: black !important;
+      }
+    }
      .users-table
      {
      //height: 300px;
